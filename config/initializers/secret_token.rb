607eadbc3bfd817835b59c43ad82f5231301ba50
hasini <hasini.psk@gmail.com>
2011-11-05 19:21:53 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyFamilyPractice::Application.config.secret_token = '328f535f016d781a823519c6db0278dd2ada69bea9ceda95ab6c36d0d1bd43d619915515c0dec21b0fe0d30ecb880fd6fae17406d67c2648047d179fdc002755'
